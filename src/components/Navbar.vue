7663a8486acb3a505d1296bacc5cbc9b2895c607
ReignLarraquel <reignlarraquel@gmail.com>
2023-09-04 22:10:43 +0800

    Change padding for navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,10 +2,10 @@
    <div class="flex flex-row bg-gray font-sana uppercase justify-between px-20 py-3 font-semibold">
       <p class="text-accent">Reign</p>
       <div>
-         <a href="/" class="px-1">Home</a>
-         <a href="/about" class="px-1">About</a>
-         <a href="/" class="px-1">Project</a>
-         <a href="/contact" class="px-1">Contact</a>
+         <a href="/" class="px-3">Home</a>
+         <a href="/about" class="px-3">About</a>
+         <a href="/" class="px-3">Project</a>
+         <a href="/contact" class="ps-3">Contact</a>
       </div>
    </div>
 </template>
