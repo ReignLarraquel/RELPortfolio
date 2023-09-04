d5d341ab1a4aec2463d1ee7e9c8c83965764f1e8
ReignLarraquel <reignlarraquel@gmail.com>
2023-09-04 22:11:17 +0800

    Add Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template class="bottom-0">
+   <div class="bg-dark_gray py-5">
+      <div class="flex flex-row justify-between px=10">
+         <div class="flex flex-row">
+            <div class="flex flex-col ps-10">
+               <div>
+                  <p>PHONE</p>
+               </div>
+               <p>+-- --- --- ----</p>
+            </div>
+            <div class="flex flex-col ps-10">
+               <div>
+                  <p>EMAIL</p>
+               </div>
+               <p>---@---.com</p>
+            </div>
+            <div class="flex flex-col ps-10">
+               <div>
+                  <p>LOCATION</p>
+               </div>
+               <p>--- --- ---</p>
+            </div>
+         </div>
+         <div class="flex flex-row pe-10">
+            
+         </div>
+      </div>
+   </div>
+   <div class="bg-accent text-xs text-black ps-10">
+      <p>Copyright © 2023 Reign Larraquel</p>
+   </div>
+</template>
